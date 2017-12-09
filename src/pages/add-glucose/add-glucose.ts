da181e400c9cee6a40ef95001fef8e9fb7cda97c
Miguel Collado <m.collado.gomez@gmail.com>
2017-12-09 09:40:03 +0100

    WIP

--- src/pages/add-glucose/add-glucose.ts
+++ src/pages/add-glucose/add-glucose.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
  * Generated class for the AddGlucosePage page.
@@ -8,13 +8,14 @@ import { NavController } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+@IonicPage()
 @Component({
   selector: 'page-add-glucose',
   templateUrl: 'add-glucose.html',
 })
 export class AddGlucosePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
--- src/utils/user-configuration.ts
+++ src/utils/user-configuration.ts
@@ -4,10 +4,18 @@ import { TranslateService } from "@ngx-translate/core";
 
 export class UserConfiguration {
     private data = {}
+
+    public language = "en";
+    public timezone = "Europe/Madrid";
+    public dateFormat = "";
     
     constructor(configurationRoot, private translate: TranslateService) {
         this.extractData(configurationRoot);
         this.refreshUserConfig();
+
+        this.language = this.getValue("dia_config__language");
+        this.timezone = this.getValue("dia_config__timezone");
+        this.dateFormat = this.getValue("dia_config__date_format");
     }
 
     updateValues(data) { // namespace_keys and values
@@ -20,6 +28,7 @@ export class UserConfiguration {
     private refreshUserConfig(){
         this.translate.use(this.getValue("dia_config__language"));
         this.translate.setDefaultLang(this.getValue("dia_config__language"));
+
     }
 
     private extractData(configurationRoot) {
