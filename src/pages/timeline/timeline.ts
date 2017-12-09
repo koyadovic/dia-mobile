42567a1526628924f80e60473c7e38c52ad23c8b
Miguel Collado <m.collado.gomez@gmail.com>
2017-12-09 13:16:20 +0100

    First testing version. Go to Add Glucose Page

--- src/pages/timeline/timeline.ts
+++ src/pages/timeline/timeline.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ConfigurationPage } from '../configuration/configuration';
 import { Observable } from 'rxjs/Observable';
@@ -14,6 +14,7 @@ import { DiaTimelineService } from '../../services/dia-timeline-service';
   templateUrl: 'timeline.html',
 })
 export class TimeLinePage {
+  @ViewChild(FabContainer) fab: FabContainer;
   private timeline$: Observable<any>;
   private userConfig: UserConfiguration;
 
@@ -38,6 +39,10 @@ export class TimeLinePage {
   addGlucose(fab: FabContainer){
     fab.close();
     this.navCtrl.push(AddGlucosePage)
+  }
+
+  cardClicked(instant) {
+    console.log(JSON.stringify(instant));
   }
 
 }
