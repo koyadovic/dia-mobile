45d4a7084f077359fcf702c76a1102ab8996a99f
Miguel Collado <m.collado.gomez@gmail.com>
2017-12-14 14:14:10 +0100

    Changes in URLs

--- src/services/dia-backend-urls.ts
+++ src/services/dia-backend-urls.ts
@@ -2,21 +2,17 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DiaBackendURL {
-  private apiHostLocal = '127.0.0.1:8000';
-  private apiHostPreProduction = 'api-test.diamobile.com';
-  private apiHostProduction = 'api.diamobile.com';
+  private apiHostLocal = 'http://127.0.0.1:8000';
+  private apiHostPreProduction = 'https://api-test.diamobile.com';
+  private apiHostProduction = 'https://api.diamobile.com';
   
-  // for production this will need to be changed too
-  // private defaultSchema = "https://"
-  // private defaultWebsocketsSchema = "wss://"
-  private defaultSchema = "http://"
-  private defaultWebsocketsSchema = "ws://"
+  private apiWSHostLocal = 'ws://127.0.0.1:8000';
+  private apiWSHostPreProduction = 'wss://api-test.diamobile.com';
+  private apiWSHostProduction = 'wss://api.diamobile.com';
 
   // Change this line for production
-  private apiHost = this.apiHostLocal;
-    
-  public baseURL = this.defaultSchema + this.apiHost;
-  public wsBaseURL = this.defaultWebsocketsSchema + this.apiHost;
+  public baseURL = this.apiHostPreProduction;
+  public wsBaseURL = this.apiWSHostPreProduction;
 
   constructor() {}
 }
