1f86b7c1444cc0c142fc85afe6202bd41181ae6a
Miguel Collado <m.collado.gomez@gmail.com>
2018-01-12 18:49:13 +0100

    Fix to be shown tabs names at the bottom translated

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -4,6 +4,7 @@ import { TimeLinePage } from '../timeline/timeline';
 import { PlanningsPage } from '../plannings/plannings';
 import { StatsPage } from '../stats/stats';
 import { TranslateService } from '@ngx-translate/core';
+import { forkJoin } from 'rxjs/observable/forkJoin';
 
 @Component({
   selector: 'page-main',
@@ -14,10 +15,25 @@ export class MainPage {
   tab2 = PlanningsPage;
   tab3 = StatsPage;
 
+  timelineTitle = 'Timeline';
+  planningsTitle = 'Plannings';
+  statsTitle = 'Stats';
 
   constructor(private navCtrl: NavController,
               private navParams: NavParams,
               private translate: TranslateService) {
+    
+    forkJoin(
+      this.translate.get('Timeline'),
+      this.translate.get('Plannings'),
+      this.translate.get('Stats'),
+    ).subscribe(
+      ([timeline, plannings, stats]) => {
+        this.timelineTitle = timeline;
+        this.planningsTitle = plannings;
+        this.statsTitle = stats;
+      }
+    )
   }
 
 
