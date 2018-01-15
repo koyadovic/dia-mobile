168af0c3bcf72a96220b292813a1b64c6ffbab70
Miguel Collado <m.collado.gomez@gmail.com>
2018-01-15 20:30:05 +0100

    Some tests with menu navigation

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -4,6 +4,8 @@ import { MainPage } from '../main/main';
 import { App } from 'ionic-angular/components/app/app';
 import { ConfigurationPage } from '../configuration/configuration';
 import { MenuController } from 'ionic-angular/components/app/menu-controller';
+import { Events } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-menu',
@@ -11,15 +13,26 @@ import { MenuController } from 'ionic-angular/components/app/menu-controller';
 })
 export class MenuPage {
   private rootPage =  MainPage;
+  private selectedIndex: number = 0;
 
   constructor(public navCtrl: NavController,
               private appCtrl: App,
               public menuCtrl: MenuController,
-              public navParams: NavParams) {}
+              public navParams: NavParams,
+              public events: Events) {
+
+    this.events.subscribe('response:change:tab', (index) => {
+      this.selectedIndex = index;
+    });
+
+  }
 
 
   goConfiguration() {
     this.appCtrl.getRootNavs()[0].push(ConfigurationPage);
   }
-            
+
+  eventGoTab(index: number){
+    this.events.publish('request:change:tab', index);
+  }
 }
