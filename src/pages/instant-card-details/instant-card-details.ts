c87db0d016bd9996ca476be824a0d0c698ecc91b
Miguel Collado <m.collado.gomez@gmail.com>
2018-03-30 16:27:12 +0200

    Delete confirmation dialog

--- src/pages/instant-card-details/instant-card-details.ts
+++ src/pages/instant-card-details/instant-card-details.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, AlertController } from 'ionic-angular';
 import { DiaTimelineService } from '../../services/dia-timeline-service';
+import { forkJoin } from 'rxjs/observable/forkJoin';
+import { TranslateService } from '@ngx-translate/core';
 
 
 @Component({
@@ -13,7 +15,9 @@ export class InstantCardDetailsPage {
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               private viewCtrl: ViewController,
-              private timelineService: DiaTimelineService) {
+              private timelineService: DiaTimelineService,
+              private translate: TranslateService,
+              private alertCtrl: AlertController) {
 
     this.instantCard = this.navParams.get("instantCard");
     console.log(this.instantCard);
@@ -24,16 +28,40 @@ export class InstantCardDetailsPage {
     this.viewCtrl.dismiss({"refresh": true, "deletedID": this.instantCard.id});
   }
   deleteInstant() {
-    // eliminarlo
-    this.timelineService.deleteInstant(this.instantCard.id, this.instantCard.signature).subscribe(
-      (resp) => {
-        this.closeWithRefresh();
-      },
-      (err) => {
-        console.error(err);
-      }
-    );
-    
+    forkJoin(
+      this.translate.get('Confirm delete'),
+      this.translate.get('Do you really want to eliminate this instant?'),
+      this.translate.get('Cancel'),
+      this.translate.get('Delete'),
+    ).subscribe(([title, mess, cancelText, deleteText]) => {
+      let alert = this.alertCtrl.create({
+        title: title,
+        message: mess,
+        buttons: [
+          {
+            text: cancelText,
+            role: 'cancel',
+            handler: () => {
+            }
+          },
+          {
+            text: deleteText,
+            handler: () => {
+              // eliminarlo
+              this.timelineService.deleteInstant(this.instantCard.id, this.instantCard.signature).subscribe(
+                (resp) => {
+                  this.closeWithRefresh();
+                },
+                (err) => {
+                  console.error(err);
+                }
+              );
+            }
+          }
+        ]
+      });
+      alert.present();
+    });
   }
 
   closeWithoutRefresh() {
