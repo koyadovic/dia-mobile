33c3bfa595427d95893b410438d433d1e677bf39
Miguel Collado <m.collado.gomez@gmail.com>
2018-11-17 17:34:05 +0100

    Simple change to select backend uri if app in browser or native phone

--- src/services/dia-backend-urls.ts
+++ src/services/dia-backend-urls.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular/platform/platform';
 
 @Injectable()
 export class DiaBackendURL {
-  private apiHostLocal = 'http://127.0.0.1:8000';
-  private apiHostPreProduction = 'https://api-test.diamobile.com';
-  private apiHostProduction = 'https://api.diamobile.com';
-  
-  // Change this line for production
-  public baseURL = this.apiHostLocal;
+    private apiHostLocal = 'http://127.0.0.1:8000';
+    private apiHostPreProduction = 'https://api-test.diamobile.com';
+    private apiHostProduction = 'https://api.diamobile.com';
+    
+    // Change this line for production
+    public baseURL = null;
 
-  constructor() {}
+    constructor(public platform: Platform) {
+        if(this.isBrowser()) {
+            this.baseURL = this.apiHostLocal;
+        } else {
+            this.baseURL = this.apiHostPreProduction;
+        }
+    }
+
+    isBrowser() {
+        return this.platform.is('core') || this.platform.is('mobileweb');
+    }
 }
